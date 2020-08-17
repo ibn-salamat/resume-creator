d6d9704aef79db2c6d58707b215d42128158602b
nurlykhan <n.salamatoff@gmail.com>
2020-08-17 19:57:54 +0600

    resume add input

--- client/src/views/CreateResume/steps/Second.js
+++ client/src/views/CreateResume/steps/Second.js
@@ -1,13 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import { useForm } from "react-hook-form";
-import { TextInput } from "grommet";
+import { TextInput, Anchor, Button } from "grommet";
 import { useStore } from "effector-react";
+import { NavLink } from "react-router-dom";
 import { $user } from "../../../store/user";
 
 export function Second() {
   const user = useStore($user);
-  console.log(user);
   const { register, handleSubmit, errors } = useForm();
 
   const onSubmit = (data) => {
@@ -20,15 +20,45 @@ export function Second() {
 
       {user.fullDataLoaded && (
         <form onSubmit={handleSubmit(onSubmit)}>
-          <div>
-            <p>Fullname</p>
+          <Field>
+            <p>Name:</p>
             <TextInput
-              name="fullname"
-              disabled
+              name="name"
               ref={register({ required: true })}
-              value="ss"
+              defaultValue={user.name}
             />
-          </div>
+          </Field>
+
+          <Field>
+            <p>Lastname:</p>
+            <TextInput
+              name="lastname"
+              ref={register({ required: true })}
+              defaultValue={user.lastname}
+            />
+          </Field>
+
+          <Field>
+            <p>Date of birth:</p>
+            <TextInput
+              name="birthday"
+              ref={register({ required: true })}
+              defaultValue={user.birthday}
+            />
+          </Field>
+
+          <Field>
+            <p>Email:</p>
+            <TextInput
+              name="email"
+              ref={register({ required: true })}
+              defaultValue={user.email}
+            />
+          </Field>
+
+          <Button primary type="submit">
+            Next
+          </Button>
         </form>
       )}
     </div>
@@ -37,4 +67,13 @@ export function Second() {
 
 const H2 = styled.h2`
   font-weight: 100;
+`;
+
+const ColoredNavLink = styled(NavLink)`
+  color: #7d4cdb;
+`;
+
+const Field = styled.div`
+  border: 1px solid red;
+  margin: 10px 0;
 `;
